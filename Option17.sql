7c7d654c2a6e93499391a21964e410e6d68da353
CoR0NaExTrA <159535808+CoR0NaExTrA@users.noreply.github.com>
2025-02-16 18:32:46 +0300

    Изменил названия полей

--- Option17.sql
+++ Option17.sql
@@ -3,9 +3,9 @@ IF NOT EXISTS (SELECT * FROM sysobjects WHERE name='Article')
 	CREATE TABLE dbo.Article(
 		article_id INT IDENTITY(1,1) NOT NULL,
 
-		article_title NVARCHAR(50) NOT NULL,
+		title NVARCHAR(50) NOT NULL,
 		article_number NVARCHAR(50) NOT NULL,
-		article_description NVARCHAR(200) NOT NULL,
+		notes NVARCHAR(200) NOT NULL,
 		maximum_sentence NVARCHAR(50) NOT NULL,
 
 		CONSTRAINT PK_article_article_id PRIMARY KEY (article_id)
@@ -15,11 +15,11 @@ IF NOT EXISTS (SELECT * FROM sysobjects WHERE name='Lawyer')
 	CREATE TABLE dbo.Lawyer(
 		lawyer_id INT IDENTITY(1,1) NOT NULL,
 
-		lawyer_name NVARCHAR(50) NOT NULL,
-		lawyer_surname NVARCHAR(50) NOT NULL,
-		lawyer_birthday NVARCHAR(50) NOT NULL,
-		lawyer_license_number NVARCHAR(50) NOT NULL,
-		lawyer_contact NVARCHAR(50) NOT NULL,
+		first_name NVARCHAR(50) NOT NULL,
+		last_name NVARCHAR(50) NOT NULL,
+		date_of_birth DATETIME NOT NULL,
+		license_number NVARCHAR(50) NOT NULL,
+		contact_info NVARCHAR(50) NOT NULL,
 
 		CONSTRAINT PK_performance_performance_id PRIMARY KEY (lawyer_id),
 	)
@@ -28,11 +28,11 @@ IF NOT EXISTS (SELECT * FROM sysobjects WHERE name='Crime')
 	CREATE TABLE dbo.Crime(
 		crime_id INT IDENTITY(1,1) NOT NULL,
 
-		crime_title NVARCHAR(50) NOT NULL,
-		crime_punishment NVARCHAR(50) NOT NULL,
+		title NVARCHAR(50) NOT NULL,
+		punishment NVARCHAR(50) NOT NULL,
+		crime_number NVARCHAR(50) NOT NULL,
 		article_id INT NOT NULL,
 		lawyer_id INT NOT NULL,
-		crime_number NVARCHAR(50) NOT NULL,
 
 		CONSTRAINT PK_crime_crime_id PRIMARY KEY (crime_id),
 		CONSTRAINT FK_crime_article_id
@@ -45,10 +45,10 @@ IF NOT EXISTS (SELECT * FROM sysobjects WHERE name='Recidivist')
 	CREATE TABLE dbo.Recidivist(
 		recidivist_id INT IDENTITY(1,1) NOT NULL,
 
-		recidivist_name NVARCHAR(50) NOT NULL,
-		recidivist_surname NVARCHAR(50) NOT NULL,
-		recidivist_birthdate DATETIME NOT NULL,
-		recidivist_gender NVARCHAR(50) NOT NULL,
+		first_name NVARCHAR(50) NOT NULL,
+		last_name NVARCHAR(50) NOT NULL,
+		date_of_birth DATETIME NOT NULL,
+		gender NVARCHAR(50) NOT NULL,
 		crime_id INT NOT NULL,
 
 		CONSTRAINT PK_recidivist_recidivist_id PRIMARY KEY (recidivist_id),
@@ -60,60 +60,14 @@ IF NOT EXISTS (SELECT * FROM sysobjects WHERE name='Incarceration')
 	CREATE TABLE dbo.Incarceration(
 		incarceration_id INT IDENTITY(1,1) NOT NULL,
 
-		incarceration_start DATETIME NOT NULL,
-		incarceration_finish DATETIME NOT NULL,
+		start_at DATETIME NOT NULL,
+		finish_at DATETIME NOT NULL,
+		prison NVARCHAR(50) NOT NULL,
 		recidivist_id INT NOT NULL,
-		incarceration_prison NVARCHAR(50) NOT NULL,
 
 		CONSTRAINT PK_incarceration_incarceration_id PRIMARY KEY (incarceration_id),
 		CONSTRAINT FK_incarceration_recidivist_id 
 			FOREIGN KEY (recidivist_id) REFERENCES dbo.Recidivist(recidivist_id)
 	)
-
-
-INSERT INTO dbo.Article (article_title, article_number, article_description, maximum_sentence)
-VALUES 
-	('Theft', '158', 'Secret theft of other people''s property', '10 years'),
-	('Fraud', '159', 'Theft of property by deception or abuse of trust', '10 years'),
-	('Bribe', '290', 'Receipt by an official personally or through an intermediary of money, securities, or other property', '4 years'),
-	('Forgery of documents', '327', 'The production, forgery, or use of forged documents for the purpose of deception', '4 years'),
-	('Hooliganism', '213', 'Gross violation of public order, expressing clear disrespect for society', '7 years'),
-	('Illegal drug trafficking', '228', 'Illegal possession, transportation, or sale of narcotic drugs', '20 years');
-
-INSERT INTO dbo.Lawyer (lawyer_name, lawyer_surname, lawyer_birthday, lawyer_license_number, lawyer_contact)
-VALUES 
-	('Kaete', 'Guinan', '06/01/1978', '895138', '4(868)897-73-67'),
-	('Assandros', 'Collins', '10/10/1962', '994678', '71(2758)766-54-91'),
-	('Natalie', 'Bieber', '28/01/2003', '239994', '1(8152)847-75-00'),
-	('Murdoch', 'Heisenberg', '06/05/1969', '552775', '5(1716)213-17-12'),
-	('Tatjana', 'Buckley', '07/10/1986', '411793', '9(811)669-84-11'),
-	('Moriko', 'Tietjens', '03/05/2000', '193447', '7(1232)584-80-79');
-
-INSERT INTO dbo.Crime (crime_title, crime_punishment, article_id, crime_number, lawyer_id)
-VALUES 
-	('Theft', '5 years', '1', '426406', '5'),
-	('Fraud', '4 years', '2', '130600', '2'),
-	('Bribe', '2 years', '3', '667972', '4'),
-	('Forgery of documents', '4 years', '4', '619259', '3'),
-	('Hooliganism', '7 years', '5', '538007', '1'),
-	('Illegal drug trafficking', '15 years', '6', '064928', '6');
-
-INSERT INTO dbo.Recidivist (recidivist_name, recidivist_surname, recidivist_birthdate, recidivist_gender, crime_id)
-VALUES 
-	('Pavel', 'Lopatinsky', '19/08/2000', 'male', '3'),
-	('Isabell', 'Cormac', '13/10/1992', 'female', '5'),
-	('Mary', 'Mori', '18/12/1995', 'female', '2'),
-	('Tristan', 'Smith', '10/06/1987', 'male', '6'),
-	('Renzo', 'Gegenbauer', '22/03/1970', 'male', '4'),
-	('Herman', 'Campbell', '04/09/1980', 'male', '1');
-
-INSERT INTO dbo.Incarceration (incarceration_start, incarceration_finish, recidivist_id, incarceration_prison)
-VALUES 
-	('16/03/2020', '16/03/2025', '6', 'Dark Canyon'),
-	('23/02/2010', '23/02/2014', '1', 'Black Crypt'),
-	('18/05/2015', '18/05/2017', '5', 'Black Crypt'),
-	('10/04/2016', '10/04/2020', '1', 'Rocky Cliff'),
-	('22/03/2014', '22/03/2021', '4', 'Dark Canyon'),
-	('27/02/2013', '27/02/2028', '3', 'Fortress of Fortua');
 
 SELECT * FROM dbo.Lawyer;
--- Option18.sql
+++ Option18.sql
@@ -3,10 +3,10 @@ IF NOT EXISTS (SELECT * FROM sysobjects WHERE name='Director')
 	CREATE TABLE dbo.Director(
 		director_id INT IDENTITY(1,1) NOT NULL,
 
-		director_name NVARCHAR(50) NOT NULL,
-		director_surname NVARCHAR(50) NOT NULL,
-		director_birthdate NVARCHAR(50) NOT NULL,
-		director_number NVARCHAR(50) NOT NULL,
+		first_name NVARCHAR(50) NOT NULL,
+		last_name NVARCHAR(50) NOT NULL,
+		date_of_birth DATETIME NOT NULL,
+		number_phone NVARCHAR(50) NOT NULL,
 
 		CONSTRAINT PK_director_director_id PRIMARY KEY (director_id)
 	)
@@ -15,11 +15,11 @@ IF NOT EXISTS (SELECT * FROM sysobjects WHERE name='Actor')
 	CREATE TABLE dbo.Actor(
 		actor_id INT IDENTITY(1,1) NOT NULL,
 
-		actor_name NVARCHAR(50) NOT NULL,
-		actor_surname NVARCHAR(50) NOT NULL,
-		actor_birthdate NVARCHAR(50) NOT NULL,
-		actor_number NVARCHAR(50) NOT NULL,
-		actor_award NVARCHAR(50) NOT NULL,
+		first_name NVARCHAR(50) NOT NULL,
+		last_name NVARCHAR(50) NOT NULL,
+		date_of_birth DATETIME NOT NULL,
+		number_phone NVARCHAR(50) NOT NULL,
+		award NVARCHAR(50) NOT NULL,
 
 		CONSTRAINT PK_actor_actor_id PRIMARY KEY (actor_id)
 	)
@@ -28,11 +28,11 @@ IF NOT EXISTS (SELECT * FROM sysobjects WHERE name='Author')
 	CREATE TABLE dbo.Author(
 		author_id INT IDENTITY(1,1) NOT NULL,
 
-		author_name NVARCHAR(50) NOT NULL,
-		author_surname NVARCHAR(50) NOT NULL,
-		author_birthdate DATETIME NOT NULL,
-		author_number NVARCHAR(50) NOT NULL,
-		author_award NVARCHAR(50) NOT NULL,
+		first_name NVARCHAR(50) NOT NULL,
+		last_name NVARCHAR(50) NOT NULL,
+		date_of_birth DATETIME NOT NULL,
+		number_phone NVARCHAR(50) NOT NULL,
+		award NVARCHAR(50) NOT NULL,
 
 		CONSTRAINT PK_author_author_id PRIMARY KEY (author_id)
 	)
@@ -42,8 +42,8 @@ IF NOT EXISTS (SELECT * FROM sysobjects WHERE name='Play')
 	CREATE TABLE dbo.Play(
 		play_id INT IDENTITY(1,1) NOT NULL,
 
-		play_name NVARCHAR(50) NOT NULL,
-		play_creating DATETIME NOT NULL,
+		title NVARCHAR(50) NOT NULL,
+		creating_at DATETIME NOT NULL,
 		author_id INT NOT NULL,
 
 		CONSTRAINT PK_play_play_id PRIMARY KEY (play_id),
@@ -56,19 +56,19 @@ IF NOT EXISTS (SELECT * FROM sysobjects WHERE name='Performance')
 	CREATE TABLE dbo.Performance(
 		performance_id INT IDENTITY(1,1) NOT NULL,
 
-		performance_name NVARCHAR(50) NOT NULL,
-		performance_type NVARCHAR(50) NOT NULL,
-		performance_premiere DATETIME NOT NULL,
+		title NVARCHAR(50) NOT NULL,
+		style NVARCHAR(50) NOT NULL,
+		premiere_at DATETIME NOT NULL,
 		actor_id INT NOT NULL,
 		director_id INT NOT NULL,
 		play_id INT NOT NULL,
 
 		CONSTRAINT PK_performance_performance_id PRIMARY KEY (performance_id),
 		CONSTRAINT FK_performance_actor_id 
-			FOREIGN KEY (performance_id) REFERENCES dbo.Actor(actor_id),
-		CONSTRAINT FK_performance_director_id 
-			FOREIGN KEY (performance_id) REFERENCES dbo.Director(director_id),
-		CONSTRAINT FK_performance_play_id 
+			FOREIGN KEY (actor_id) REFERENCES dbo.Actor(actor_id),
+		CONSTRAINT FK_performance_author_id 
+			FOREIGN KEY (director_id) REFERENCES dbo.Director(director_id),
+		CONSTRAINT FK_performance_play_id
 			FOREIGN KEY (play_id) REFERENCES dbo.Play(play_id)
 	)
 
@@ -76,69 +76,14 @@ IF NOT EXISTS (SELECT * FROM sysobjects WHERE name='Theatre')
 	CREATE TABLE dbo.Theatre(
 		theatre_id INT IDENTITY(1,1) NOT NULL,
 
-		theatre_name NVARCHAR(50) NOT NULL,
-		theatre_type NVARCHAR(50) NOT NULL,
-		theatre_number NVARCHAR(50) NOT NULL,
+		title NVARCHAR(50) NOT NULL,
+		style NVARCHAR(50) NOT NULL,
+		number_phone NVARCHAR(50) NOT NULL,
 		performance_id INT NOT NULL,
 
 		CONSTRAINT PK_theatre_theatre_id PRIMARY KEY (theatre_id),
-		CONSTRAINT FK_theatre_performance_id 
+		CONSTRAINT FK_theatre_performance_id
 			FOREIGN KEY (performance_id) REFERENCES dbo.Performance(performance_id)
 	)
-
-INSERT INTO dbo.Director (director_name,  director_surname, director_birthdate, director_number)
-VALUES 
-	('Lennon', 'Griffiths', '06/01/1978', '895138'),
-	('Rimma', 'Terentyeva', '10/10/1962', '994678'),
-	('Miroslav', 'Komarov', '28/01/2003', '239994'),
-	('Siegfried', 'Fabry', '06/05/1969', '552775'),
-	('Brutus', 'Lode', '07/10/1986', '411793'),
-	('Adeia', 'Wilkinson', '03/05/2000', '193447');
-
-INSERT INTO dbo.Actor (actor_name, actor_surname, actor_birthdate, actor_number, actor_award)
-VALUES 
-	('Hedi', 'Milnor', '24/12/1969', '426406', 'Golden Mask 2020'),
-	('Christof', 'Huygens', '14/08/1992', '130600', 'Breakthrough of the year 2018'),
-	('Aretha', 'Germanova', '01/11/2002', '667972', 'Actor of the Year 2020'),
-	('Birgith', 'Sankt', '29/10/1971', '619259', 'Actor of the Year 2018'),
-	('Nahoko', 'Gregory', '07/03/1978', '538007', 'Breakthrough of the year 2023'),
-	('Robert', 'Hamermesh', '28/01/1988', '064928', 'Golden applause 2023');
-
-INSERT INTO dbo.Author (author_name, author_surname, author_birthdate, author_number, author_award)
-VALUES 
-	('Sylvia', 'Peclet', '19/08/1965', '774246', 'Golden Feather 2024'),
-	('Andrew', 'Porter', '13/10/1972', '689285', 'Golden Feather 2015'),
-	('Amy', 'Truesdell', '18/12/1959', '177611', 'Generational Inspiration 2015'),
-	('Zinaida', 'Rodionova', '10/06/1978', '507513', 'Play of the Year 2023'),
-	('Sam', 'Goldman', '22/03/1970', '476195', 'Literary masterpiece 2020'),
-	('Darius', 'Vallerius', '04/09/1962', '203569', 'Literary masterpiece 2017');
-
-INSERT INTO dbo.Performance (performance_name, performance_type, performance_premiere, actor_id, director_id)
-VALUES 
-	('Shadow Dance', 'drama', '16/03/2025', '6', '4'),
-	('Eternal Game', 'ballet', '23/02/2025', '1', '6'),
-	('Labyrinths of Fate', 'musical', '18/05/2025', '5', '1'),
-	('Keepers of Secrets', 'drama', '10/04/2025', '1', '3'),
-	('Beyond the Past', 'comedy', '22/03/2025', '4', '1'),
-	('Faces Without Faces', 'comedy', '27/02/2025', '3', '2');
-
-INSERT INTO dbo.Play (play_name, play_creating, author_id)
-VALUES 
-	('Light and Darkness', '22/07/1989', '1'),
-	('Web of Illusions', '07/03/1984', '6'),
-	('Peaks of Hope', '13/04/1989', '5'),
-	('Echo of the Past', '30/08/1998', '2'),
-	('On the Wings of A Dream', '17/02/1982', '3'),
-	('Intricate Pattern', '11/05/1999', '4');
-
-INSERT INTO dbo.Theatre (theatre_name, theatre_type, theatre_number, performance_id)
-VALUES 
-	('Art Palace', 'dramatic', '459499', '3'),
-	('Magical Scene', 'puppet', '993927', '1'),
-	('Paradise Theater', 'opera and ballet', '560045', '4'),
-	('Epicenter Theater', 'comedy', '226727', '5'),
-	('Drama Academy', 'dramatic', '724000', '2'),
-	('Ramp Light', 'comedy', '137476', '6');
-
 
 SELECT * FROM dbo.Actor;
